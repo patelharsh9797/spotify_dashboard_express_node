3ccec3c3fa48d22a96ead535a791897fbf1edf46
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-07-20 11:56:20 +0530

    added /login & /callback routes

--- index.js
+++ index.js
@@ -1,22 +1,139 @@
+require("dotenv").config();
+const querystring = require("node:querystring");
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
+const port = 8888;
 
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI;
+const stateKey = "spotify_auth_state";
+
+//--------------------------------
+// TODO Utils Functions
+//--------------------------------
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+//--------------------------------
+// TODO Routes && Methods
+//--------------------------------
 app.get("/", (req, res) => {
   const data = {
     name: "Harsh",
     isAwesome: true,
   };
+
   res.json(data);
 });
 
-app.get("/awesome-generator", (req, res) => {
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? "really" : "not"} awesome`);
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
+  const queryParams = querystring.stringify({
+    response_type: "code",
+    client_id,
+    redirect_uri,
+    state,
+    scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-const port = 3000;
+app.get("/callback", (req, res) => {
+  const code = req.query.code || null;
 
+  axios({
+    method: "POST",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri,
+    }),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${client_id}:${client_secret}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        // const { access_token, token_type } = response.data;
+
+        // axios
+        //   .get("https://api.spotify.com/v1/me", {
+        //     headers: {
+        //       Authorization: `${token_type} ${access_token}`,
+        //     },
+        //   })
+        //   .then((response) => {
+        //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        //   })
+        //   .catch((error) => res.send(error));
+
+        const { refresh_token } = response.data;
+
+        axios
+          .get(
+            `http://localhost:8888/refresh_token?refresh_token=${refresh_token}`
+          )
+          .then((response) => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch((error) => {
+            res.send(error);
+          });
+      } else {
+        res.send(response);
+      }
+    })
+    .catch((error) => res.send(error));
+});
+
+app.get("/refresh_token", (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: "POST",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token,
+    }),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${client_id}:${client_secret}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => res.send(response.data))
+    .catch((error) => res.send(error));
+});
+
+//--------------------------------
+// TODO Port listening
+//--------------------------------
 app.listen(port, () => {
   console.log(`Express running at : http://localhost:${port}`);
 });
