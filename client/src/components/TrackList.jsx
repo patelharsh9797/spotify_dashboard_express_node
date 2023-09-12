244b524c783cdadd1ad4f1cb7bc6ef752db4a8c7
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-09-12 11:41:20 +0530

    added individual playlist page

--- client/src/components/TrackList.jsx
+++ client/src/components/TrackList.jsx
@@ -7,33 +7,51 @@ const TrackList = ({ tracks }) => (
       <StyledTrackList>
         {tracks.map((track, i) => (
           <li className="track__item" key={i}>
-            <div className="track__item__num">{i + 1}</div>
-            <div className="track__item__title-group">
-              {track.album.images.length && track.album.images[2] && (
-                <div className="track__item__img">
-                  <img src={track.album.images[2].url} alt={track.name} />
+            {track ? (
+              <>
+                <div className="track__item__num">{i + 1}</div>
+                <div className="track__item__title-group">
+                  {track.album.images.length > 0 && track.album.images[2] && (
+                    <div className="track__item__img">
+                      <img src={track.album.images[2].url} alt={track.name} />
+                    </div>
+                  )}
+                  <div className="track__item__name-artist">
+                    <div className="track__item__name overflow-ellipsis">
+                      {track.name}
+                    </div>
+                    <div className="track__item__artist overflow-ellipsis">
+                      {track.artists.map((artist, i) => (
+                        <span key={i}>
+                          {artist.name}
+                          {i !== track.artists.length - 1 && ", "}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-              )}
-              <div className="track__item__name-artist">
-                <div className="track__item__name overflow-ellipsis">
-                  {track.name}
+                <div className="track__item__album overflow-ellipsis">
+                  {track.album.name}
                 </div>
-                <div className="track__item__artist overflow-ellipsis">
-                  {track.artists.map((artist, i) => (
-                    <span key={i}>
-                      {artist.name}
-                      {i !== track.artists.length - 1 && ", "}
-                    </span>
-                  ))}
+                <div className="track__item__duration">
+                  {formatDuration(track.duration_ms)}
                 </div>
-              </div>
-            </div>
-            <div className="track__item__album overflow-ellipsis">
-              {track.album.name}
-            </div>
-            <div className="track__item__duration">
-              {formatDuration(track.duration_ms)}
-            </div>
+              </>
+            ) : (
+              <>
+                <div className="track__item__num">{i + 1}</div>
+                <div className="track__item__title-group">
+                  <div className="track__item__name-artist">
+                    <div className="track__item__name overflow-ellipsis">
+                      "No track data found"
+                    </div>
+                    <div className="track__item__artist overflow-ellipsis"></div>
+                  </div>
+                </div>
+                <div className="track__item__album overflow-ellipsis"></div>
+                <div className="track__item__duration"></div>
+              </>
+            )}
           </li>
         ))}
       </StyledTrackList>
